830ca290e5f8d00c9aa849f94491f8cabb6f7880
Grant Hesketh <ghesketh76@gmail.com>
2020-10-29 19:32:10 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -23,9 +23,8 @@
 #  puts "Stay home and read Hegel."
 #end
 
-current_time = Time.now 
-current_time = current_time.to_i 
-if current_time % 2 == 0 
+
+if Time.now.to_i % 2 == 0 
   puts "Even!"
 else
   puts "Odd!"
