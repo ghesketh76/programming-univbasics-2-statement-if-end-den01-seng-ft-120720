923454c4d148f5c7e04ea677d200ef0bd3644a5f
Grant Hesketh <ghesketh76@gmail.com>
2020-10-29 19:33:51 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -24,7 +24,16 @@
 #end
 
 
-if Time.now.to_i % 2 == 0 
+#if Time.now.to_i % 2 == 0 
+#  puts "Even!"
+#else
+#  puts "Odd!"
+#end
+
+current_time =Time.now
+current_time = current_time.to_i 
+
+if current_time.even?
   puts "Even!"
 else
   puts "Odd!"
